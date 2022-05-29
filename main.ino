7ebc2a514c7bf1f8eaa8eee753e8b17496e665e8
alejandro945 <alejo8677@gmail.com>
2022-05-29 14:41:08 -0500

    :cat: Final of code implementation

--- main.ino
+++ main.ino
@@ -1,48 +1,68 @@
+//Libraries
 #include <Wire.h>
 #include "Adafruit_TCS34725.h"
 #include <pt.h>
+/*100 palabras Example for testing no se soportan las tildes ni ñ
+  Una amiga mia se compro una comadreja, que resulto ser muy caprichosa. El bicho no quiere estar en la jaula, asi que campa a sus anchas todo el dia.
+  Tampoco quiere comida de lata, sino solo refinados guisos, y se enfada si la comida no esta a tiempo. No muerde, pero muestra los dientes.
+  Cuando ella llega del trabajo, las botellas de licor estan vacias, y la comadreja duerme en el sofa, con la tele encendida, toda la casa sucia y revuelta.
+*/
+//Basic Setup
 /* Download the library from http://dunkels.com/adam/pt/download.html then extract tar.gz in libraries arduino documents folder and delete 3 examples files*/
 /*
-   Demolador Manual Config
-   Connect SCL    to analog 5
-   Connect SDA    to analog 4
-   Connect 3V3    to 3.3V DC
-   Connect GROUND to common ground
-   Connect LED to GROUND
-   Connect v od LED to GROUND and set led pins
+  Ambos nodos deben conocer el app mode en que se operan antes de iniciar!
+  Demolador Manual Config
+  Connect SCL    to analog 5
+  Connect SDA    to analog 4
+  Connect VIN    to 5V DC INO
+  Connect GROUND to common ground
+  Connect LED to GROUND
+  Connect v od LED to GROUND and set led pins
   /*
-         R     G     B
+    R     G     B
   Inicio 255   255   0    -------> +/- 50% E
   White  255   255   255  -> 00 -> 30% E
   Blue   0     0     255  -> 01 -> 30% E
   Green  0     255   0    -> 10 -> 30% E
   Red    255   0     0    -> 11 -> 30% E
-  Final  255  0  255  -------> +/- 50% E
+  Final  255   0     255  -------> +/- 50% E
 */
-
-/* Initialise with default values (int time = 2.4ms, gain = 1x) */
+/* Initialise with default values (int time = 2.4ms, gain = 1x) "Deprecated" */
 // Adafruit_TCS34725 tcs = Adafruit_TCS34725();
 /* Initialise with specific int time and gain values */
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_614MS, TCS34725_GAIN_1X);
-struct pt demThread;
-struct pt ledThread;
-/* Const Configuration*/
+/* Thread Vareables Declaración */
+static struct pt demThread;
+static struct pt ledThread;
+static struct pt transThread;
+/* Port Const Configuration*/
 const byte RED = 9; //RED OUTPUT
 const byte BLUE = 10; //BLUE OUTPUT
 const byte GREEN = 11; //GREEN OUTPUT
 /* Global Variables Configuration*/
+boolean isSending = false;
+boolean isSensorReading = false;
 String dataSent = "";
 String textSent = "";
-boolean isSensorReading = false;
+String emisorData = "";
 String initialNodeType = ""; //Emisor or Receptor
 String appMode = "";
-//Usefull just for Bloque Case
+/*Usefull just for Bloque Case*/
 int characterCounter = 0;
 int checkSumCounter = 0;
-//---------------------------------------------EMISOR----------------------------------------
-void setup(void) {
-  Serial.begin(9600); // enable serial output for debugging
-  //Salidas digitales
+/*Usefull just for Eco Case*/
+int currentIndex = -1;
+String charSent = "";
+boolean already = false;
+//---------------------------------------------ARDUINO SET AND LOOP----------------------------------------
+void setup() {
+  //Initialise photothreads variables
+  PT_INIT(&demThread);
+  PT_INIT(&ledThread);
+  PT_INIT(&transThread);
+  //Enable serial output for debugging
+  Serial.begin(9600);
+  //Digital Outputs
   pinMode(RED, OUTPUT);
   pinMode(BLUE, OUTPUT);
   pinMode(GREEN, OUTPUT);
@@ -54,36 +74,81 @@ void setup(void) {
     Serial.println("No TCS34725 found ... check your connections");
     while (1);
   }
-  PT_INIT(&demThread);
-  PT_INIT(&ledThread);
 }
 
 //Bucle repetitivo tanto para el emisor como para un receptor modelado como una misma entidad
 void loop(void) {
   loopEmisor(&ledThread);
   loopDemolator(&demThread);
-
+  //Breaks the threads :)
+  loopTransmision(&transThread);
 }
 
+//---------------------------------------------EMISOR----------------------------------------
 void loopEmisor(struct pt *pt) {
   PT_BEGIN(pt);
-  while (true) {
+  static unsigned long timestamp = 0;
+  do {
     appCases();
-  }
+    PT_WAIT_UNTIL(pt, millis() - timestamp > 50 );
+    timestamp = millis();
+  } while (true);
   PT_END(pt);
 }
 
 void loopDemolator(struct pt *pt) {
   PT_BEGIN(pt);
-  while (true) {
+  static unsigned long timestamp = 0;
+  do {
     demodulatorObserver();
-  }
+    PT_WAIT_UNTIL(pt, millis() - timestamp > 617 );
+    timestamp = millis();
+  } while (true);
   PT_END(pt);
+}
+
+void loopTransmision(struct pt *pt) {
+  PT_BEGIN(pt);
+  static unsigned long timestamp = 0;
+  do {
+    PT_WAIT_UNTIL(pt, millis() - timestamp > 50 );
+    timestamp = millis();
+    if (emisorData != "" && !isSending) {
+
+      //Config
+      isSending = true;
+      Serial.println("App mode " + appMode);
+
+      //Setting node type
+      if (initialNodeType == "") {
+        initialNodeType = "Emisor";
+      }
+
+      //Chat Estetica
+      if (appMode == "%") {
+        Serial.println("[Envía] Canal " + initialNodeType + ": " + emisorData);
+      }
+
+      //LED Transmision for chat and Bloque
+      if (appMode != "$") {
+        transmition(emisorData);
+      } else if (!already) {
+        //Precondición solo se puede enviar por el monitor una vez :(
+        already = true;
+        ecoTransmition(true);
+      }
+
+      //Clear Data
+      isSending = false;
+      emisorData = "";
+    }
+  } while (true);
+  PT_END(pt)
 }
 
 //Ambos deben conocer el modo en que se esta operan
 void appCases() {
-  while (appMode == "") {
+  if (appMode == "") {
     String mode = dataReading();
     if (mode == "%") {
       appMode = "%";
@@ -94,24 +159,19 @@ void appCases() {
     } else if (mode != "") {
       Serial.println("Ingrese % para chat, Ingrese & para bloque, Ingrese $ para eco");
     }
+  } else {
+    getData();
   }
-  //App mode already set we continue reading data in emisor module, receptor only gonna be a suscriber
-  String emisorData = dataReading();
-  while (emisorData == "") {
-    emisorData = dataReading();
-  }
-  Serial.println("App mode " + appMode);
-  //Setting node type
-  if (initialNodeType == "") {
-    initialNodeType = "Emisor";
-  }
-  //Chat Estetica
-  if (appMode == "%") {
-    Serial.println("Canal " + initialNodeType + ": " + emisorData);
-  }
-  transmition(emisorData);
 }
 
+//App mode already set we continue reading data in emisor module, receptor only gonna be a suscriber
+void getData() {
+  if (emisorData == "") {
+    emisorData = dataReading();
+  }
+}
+
+//Auxiliar Method
 String dataReading() {
   String data = "";
   if (Serial.available()) {
@@ -120,6 +180,7 @@ String dataReading() {
   return data;
 }
 
+//2 Bits Led Transmision
 void transmition(String data) {
   transmitionCentinel(true);
   String letterToBin = "0";
@@ -135,23 +196,44 @@ void transmition(String data) {
   transmitionCentinel(false);
 }
 
+//Envío por caracter condicional
+void ecoTransmition(boolean increase) {
+  String data = (initialNodeType == "Receptor") ? textSent : emisorData;
+  if (increase) {
+    currentIndex++;
+  }
+  if (initialNodeType == "Emisor") {
+    charSent = data.charAt(currentIndex);
+  }
+  transmitionCentinel(true);
+  String letterToBin = "0";
+  char letter = data.charAt(currentIndex);
+  if (int(letter) <= 63) {
+    letterToBin += "0";
+  }
+  letterToBin += String(letter, BIN);
+  sendColor(letterToBin);
+  letterToBin = "0";
+  transmitionCentinel(false);
+}
+
+//Inicio y Fin de carrera
 void transmitionCentinel(boolean isInit) {
-  static long t = 0;
   if (isInit) {
     digitalWrite(RED, HIGH);
     digitalWrite(GREEN, HIGH);
-    t = millis();
-    //Delay from library
+    delay(635);
   } else {
     digitalWrite(RED, HIGH);
     digitalWrite(BLUE, HIGH);
-    t = millis();
+    delay(635);
   }
   turnOff();
 }
 
+//Extract 2 bits of de byte ascci character
 void sendColor(String bin) {
-  Serial.println(bin);
+  Serial.println(bin); //Testing
   String props = "";
   props += bin.charAt(0);
   props += bin.charAt(1);
@@ -172,34 +254,33 @@ void sendColor(String bin) {
   props += bin.charAt(7);
   colorCases(props);
   turnOff();
-  //Final de carrera
 }
 
+//Sending led Color using digital const outputs
 void colorCases(String twoBits) {
-  static long t = 0;
   if (twoBits.compareTo("00") == 0) {
     digitalWrite(RED, HIGH);
     digitalWrite(BLUE, HIGH);
     digitalWrite(GREEN, HIGH);
-    t = millis();
+    delay(635);
   } else if (twoBits.compareTo("01") == 0) {
     digitalWrite(BLUE, HIGH);
-    t = millis();
+    delay(635);
   } else if (twoBits.compareTo("10") == 0) {
     digitalWrite(GREEN, HIGH);
-    t = millis();
+    delay(635);
   } else {
     digitalWrite(RED, HIGH);
-    t = millis();
+    delay(635);
   }
 }
 
+// Turning off de leds
 void turnOff() {
   digitalWrite(RED, LOW);
   digitalWrite(BLUE, LOW);
   digitalWrite(GREEN, LOW);
 }
-
 
 //---------------------------------------------RECEPTOR----------------------------------------
 //Lectura constante para los receptores que estan esperendo a leer el inicio de carrera
@@ -208,73 +289,14 @@ void demodulatorObserver() {
   tcs.getRawData(&r, &g, &b, &c);
   /*Parsing to 8 bits*/
   r /= 256; g /= 256; b /= 256;
-  Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
-  Serial.println(" ");
-  //Validar Inicio
-  if (r > 150 && g > 150 && !isSensorReading) {
-    isSensorReading = true;
-    //Setting initial node type
-    if (initialNodeType == "") {
-      initialNodeType = "Receptor";
-    }
-  }
-  //Validar Fin
-  if (r > 150 && b > 150 && isSensorReading) {
-    isSensorReading = false;
-  }
-  //Leer señales electricas transmitidas por la fibra optica
-  if (isSensorReading) {
-    demodulatorReading(r, g, b);
-  }
-  //Receptor cases
-  if (!isSensorReading && textSent != "") {
-    receptorCases();
-  }
-}
-
-void demodulatorReading(int r, int g, int b) {
-  Serial.println(dataSent); //For testing porpouses
-  bitCases(r, g, b, dataSent);
-  if (dataSent.length() == 8) {
-    //---------------------Bloque Funcionamiento----------------------
-    characterCounter++;
-    checkSumCounter += 8;
-    if (appMode == "&") {
-      receptorBloque(false);
-    }
-    //---------------------Bloque Funcionamiento----------------------
-    Serial.println(valorAscii(dataSent)); //For testing porpouses letter
-    textSent += valorAscii(dataSent);
-    dataSent = "";
-  }
-  //For testing propositos
-  Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
-  Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
-  Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
-  Serial.println(" ");
-}
-
-void bitCases(int R, int G, int B, String current) {
-  if (R > 150 && G > 150 && B > 150) { //WHITE
-    dataSent += "00";
-  } else if (B > 150) { //BLUE
-    dataSent += "01";
-  } else if (G > 150) { //GREEN
-    dataSent += "10";
-  } else if (R > 150) { //RED
-    dataSent += "11";
-  }
-}
-
-//Regresa una cadena binaria en su correspondiente valor ascii
-char valorAscii(String CadenaBinaria)
-{
-  int Multiplo = 1;
-  char Res = 0;
-  char Bit;
-  for (int Cont = CadenaBinaria.length() - 1; Cont >= 0; Cont--)
-  {
-    Bit = CadenaBinaria.charAt(Cont);              //Obtengo el bit que se esta manipulando
+  /*Testing*/
+  Serial.println(r, DEC);
+  //Maxim
+  float Maximum;
+  Maximum = max(r, g);
+  Maximum = max(Maximum, b);
+    //Obtengo el bit que se esta manipulando
+    Bit = CadenaBinaria.charAt(Cont);
     if (Bit == '1')
       Res = Res + Multiplo;
     Multiplo = Multiplo * 2;
@@ -287,9 +309,17 @@ void receptorCases() {
   if (appMode == "&") {
     receptorBloque(true);
   } else if (appMode == "$") {
-    transmition(textSent);
+    if (initialNodeType == "Receptor") {
+      ecoTransmition(true);
+    } else if (charSent == textSent) {
+      ecoTransmition(true);
+    } else {
+      ecoTransmition(false);
+    }
   } else {
-    Serial.println("Canal " + appMode + ": " + textSent);
+    Serial.println("[Recibe] Canal " + appMode + ": " + textSent);
+
+
   }
 }
 
